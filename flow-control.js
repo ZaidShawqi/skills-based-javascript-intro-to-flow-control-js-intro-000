b28e357606d89b90107035c9048dd9031cb00d06
Zaid Shawqi <zeid.shawqi@gmail.com>
2020-04-03 20:52:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,15 @@
 function basicTeenager(age) {
 
-var basicTeenager = age
-
-if ( age >= 13 && age <= 19 ) {
-console.log('You are a teenager!')
+if ( age <= 13 && age >= 19 ) {
+return ('You are a teenager!')
 }
+
 
 
 function teenager(age) {
 
 if ( age < 13 && age > 19 ) {
-    console.log('You are not a teenager!')
+    return ('You are not a teenager!')
 }
 
 function ageChecker(age) {
